f8a18c2c6b4ba19776a37cdb4931180fa936507c
vikas thakur <vikas098thakur@gmail.com>
2025-11-22 11:12:45 +0530

    Delete backend directory

--- backend/routes/questionRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { getQuestions, evaluateAnswer } from '../controllers/questionController.js';
-
-router.route('/').get(getQuestions);
-router.route('/evaluate').post(evaluateAnswer);
-
-export default router;
